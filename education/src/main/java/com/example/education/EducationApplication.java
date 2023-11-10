e2866016a70dba4bbf27318bdad052d19b71e0d2
Santiago Mosquera Mapura <s.mosquera@globant.com>
2023-11-10 10:05:43 -0500

    Adding eureka-server and clients to microservices

--- education/src/main/java/com/example/education/EducationApplication.java
+++ education/src/main/java/com/example/education/EducationApplication.java
@@ -2,7 +2,9 @@ package com.example.education;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class EducationApplication {
 
